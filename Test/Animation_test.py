4d306344746065be61d1b7ad277bfd4781012d99
Sharninjak <798909776@qq.com>
2024-08-01 13:52:44 +0800

    finish resnet for mnist, but leave some function to complete

--- Test/Animation_test.py
+++ Test/Animation_test.py
@@ -1,21 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 fig = plt.figure()
 ax = fig.subplots()
 
 t=np.linspace(0,10,100)
 y=np.sin(t)
+ax.axis([0,10,0,2])
 ax.set_aspect(3)
-ax.plot(t,y,'--',c='gray')
-line=ax.plot(t,y,c='C2')
 
-def update(i):  #帧更新函数
-    global t    #直接引用全局变量，也可以通过函数的frames或fargs参数传递。
-    t+=0.1
+while True:
+    ax.plot(t,y)
+    plt.pause(0.1)  #显示图形并暂停。注意用time.sleep不起作用
+    ax.cla()        #清除图形
+    t+=np.pi/30     #更新数据
     y=np.sin(t)
-    line[0].set_ydata(y)
-    return line
-
-ani=FuncAnimation(fig,update,interval=100) #绘制动画
-plt.show() #显示动画
